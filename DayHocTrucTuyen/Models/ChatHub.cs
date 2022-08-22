0da8c54619d461502f07ad873e3145c2247ac73d
Khánh Băng <trieukhanhbang123@gmail.com>
2022-08-22 15:25:05 +0700

    Thêm các tính năng cơ bản của chat.

--- DayHocTrucTuyen/Models/ChatHub.cs
+++ DayHocTrucTuyen/Models/ChatHub.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using DayHocTrucTuyen.Models.Entities;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace DayHocTrucTuyen.Models
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string nick, string message)
+        DayHocTrucTuyenContext db = new DayHocTrucTuyenContext();
+        static List<UserConnect> ConnectedUsers = new List<UserConnect>();
+
+        public async Task SendAll(string nick, string message)
         {
             await Clients.All.SendAsync("Send", nick, message);
+        }
+
+        public async Task TestA()
+        {
+            await Clients.All.SendAsync("Send", "Tin nhắn Test", "Nội dung");
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var user = db.NguoiDungs.FirstOrDefault(x => x.MaNd == ((ClaimsIdentity)Context.User.Identity).Claims.First().Value);
+            //await Clients.AllExcept(Context.ConnectionId).SendAsync("Send", user.getName(), "Đã kết nối");
+
+            var id = Context.ConnectionId;
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                ConnectedUsers.Add(new UserConnect { ConnectionId = id, MaNd = user.MaNd, UserName = user.getName() });
+
+                await Clients.AllExcept(Context.ConnectionId).SendAsync("Send", user.getName(), "Đã kết nối");
+                await Clients.Client(Context.ConnectionId).SendAsync("Send", "Hiện có", ConnectedUsers.Count);
+                await base.OnConnectedAsync();
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (exception == null)
+            {
+                var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+
+                ConnectedUsers.Remove(item);
+                await SendAll(item.UserName, "Đã rời khỏi cuộc trò chuyện. Còn " + ConnectedUsers.Count);
+            }
+
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
---
+++ DayHocTrucTuyen/Models/UserConnect.cs
@@ -0,0 +1,9 @@
+﻿namespace DayHocTrucTuyen.Models
+{
+    public class UserConnect
+    {
+        public string ConnectionId { get; set; }
+        public string MaNd { get; set; }
+        public string UserName { get; set; }
+    }
+}
