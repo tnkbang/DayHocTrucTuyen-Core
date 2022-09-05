74c2550ee01392d39af0ad4197d0a72ca762f911
Khánh Băng <trieukhanhbang123@gmail.com>
2022-09-05 13:06:29 +0700

    Cập nhật tính năng nâng cấp tài khoản.

--- DayHocTrucTuyen/Areas/Payment/Controllers/MoMoController.cs
+++ DayHocTrucTuyen/Areas/Payment/Controllers/MoMoController.cs
@@ -1,8 +1,10 @@
-﻿using DayHocTrucTuyen.Models.Entities;
+﻿using DayHocTrucTuyen.Areas.Payment.Models;
+using DayHocTrucTuyen.Models.Entities;
 using DayHocTrucTuyen.Models.Payment.Momo;
 using DayHocTrucTuyen.Models.Payment.MoMo;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Net;
 
@@ -15,6 +17,7 @@ namespace DayHocTrucTuyen.Areas.Payment.Controllers
     public class MoMoController : Controller
     {
         DayHocTrucTuyenContext db = new DayHocTrucTuyenContext();
+        static List<UserPayment> userPayments = new List<UserPayment>();
 
         public IActionResult Pay(string pak, string money)
         {
@@ -26,7 +29,7 @@ namespace DayHocTrucTuyen.Areas.Payment.Controllers
             string orderInfo = pak;
 
             string redirectUrl = "https://localhost:44354/Profile/Info?id=" + User.Claims.First().Value;
-            string ipnUrl = "https://localhost:44354/Payment/MoMo/SavePay";
+            string ipnUrl = "https://cead-2402-800-63b5-a749-b0f8-561e-a288-b0e8.ap.ngrok.io/Payment/MoMo/SavePay";
             string requestType = "captureWallet";
 
             string amount = money;
@@ -71,37 +74,57 @@ namespace DayHocTrucTuyen.Areas.Payment.Controllers
             };
 
             string responseFromMomo = PaymentRequest.sendPaymentRequest(endpoint, message.ToString());
-
             JObject jmessage = JObject.Parse(responseFromMomo);
+
+            //Thêm người dùng đang thực hiện giao dịch vào danh sách để tiến hành lưu csdl khi thành công
+            userPayments.Add(new UserPayment
+            {
+                id = orderId,
+                maNd = User.Claims.First().Value,
+                money = double.Parse(money)
+            });
 
             return Redirect(jmessage.GetValue("payUrl").ToString());
         }
 
         [HttpPost]
-        public void SavePay(MoMoResult result)
+        [AllowAnonymous]
+        public void SavePay([FromBody] object requestData)
         {
-            var pak = db.GoiNangCaps.FirstOrDefault(x => x.GiaTien.ToString().Equals(result.amount));
+            JObject jmessage = JsonConvert.DeserializeObject<dynamic>(requestData.ToString());
 
-            if (pak != null)
+            //Lấy người dùng đang thực hiện giao dịch
+            var user = userPayments.FirstOrDefault(x => x.id.Equals(jmessage.GetValue("orderId").ToString()));
+
+            //Nếu giao dịch thành công
+            if (jmessage.GetValue("resultCode").ToString().Equals("0"))
             {
-                var upgrade = db.TrangThaiNangCaps.FirstOrDefault(x => x.MaNd == User.Claims.First().Value);
+                var pak = db.GoiNangCaps.FirstOrDefault(x => x.GiaTien == user.money);
 
-                if (upgrade == null)
+                if (pak != null)
                 {
-                    TrangThaiNangCap newUG = new TrangThaiNangCap();
-                    newUG.MaNd = User.Claims.First().Value;
-                    newUG.NgayDangKy = DateTime.Now;
-                    newUG.MaGoi = pak.MaGoi;
+                    var upgrade = db.TrangThaiNangCaps.FirstOrDefault(x => x.MaNd == user.maNd);
 
-                    db.TrangThaiNangCaps.Add(newUG);
-                }
-                else
-                {
-                    upgrade.NgayDangKy = DateTime.Now;
-                }
+                    if (upgrade == null)
+                    {
+                        TrangThaiNangCap newUG = new TrangThaiNangCap();
+                        newUG.MaNd = user.maNd;
+                        newUG.NgayDangKy = DateTime.Now;
+                        newUG.MaGoi = pak.MaGoi;
 
-                db.SaveChanges();
+                        db.TrangThaiNangCaps.Add(newUG);
+                    }
+                    else
+                    {
+                        upgrade.MaGoi = pak.MaGoi;
+                        upgrade.NgayDangKy = DateTime.Now;
+                    }
+
+                    db.SaveChanges();
+                }
             }
+
+            userPayments.Remove(user);
 
             base.Response.StatusCode = (int)HttpStatusCode.NoContent;
         }
