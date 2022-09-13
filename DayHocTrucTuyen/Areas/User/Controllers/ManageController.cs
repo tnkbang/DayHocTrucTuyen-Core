4f8133f5e1f1e7546d8c3b7787294d8c8846ac42
Khánh Băng <trieukhanhbang123@gmail.com>
2022-09-13 22:09:24 +0700

    Thêm controller quản lý tài khoản của người dùng.

---
+++ DayHocTrucTuyen/Areas/User/Controllers/ManageController.cs
@@ -0,0 +1,19 @@
+﻿using DayHocTrucTuyen.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayHocTrucTuyen.Areas.User.Controllers
+{
+    [Area(nameof(User))]
+    [Route("User/[controller]/[action]")]
+    [Authorize]
+    public class ManageController : Controller
+    {
+        DayHocTrucTuyenContext db = new DayHocTrucTuyenContext();
+        public IActionResult Index()
+        {
+            var user = db.NguoiDungs.FirstOrDefault(x => x.MaNd == User.Claims.First().Value);
+            return View(user);
+        }
+    }
+}
