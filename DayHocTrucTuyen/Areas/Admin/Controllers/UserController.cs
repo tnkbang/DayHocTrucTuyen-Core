3da6f7676ccd08fbecb71944b85c33606095c09e
Khánh Băng <trieukhanhbang123@gmail.com>
2022-09-30 23:15:10 +0700

    Add controller user in admin

---
+++ DayHocTrucTuyen/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using DayHocTrucTuyen.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DayHocTrucTuyen.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    [Route("Admin/[controller]/[action]")]
+    [Authorize(Roles = "01")]
+    public class UserController : Controller
+    {
+        DayHocTrucTuyenContext db = new DayHocTrucTuyenContext();
+
+        public IActionResult List()
+        {
+            var mems = db.NguoiDungs.Where(x => x.MaLoai != "01").ToList();
+            return View(mems);
+        }
+    }
+}
