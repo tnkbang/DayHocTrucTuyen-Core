67d85bf7c31e7f71c8616cc116ddf2f603af8f4d
Khánh Băng <trieukhanhbang123@gmail.com>
2022-11-04 18:10:35 +0700

    Phân trang với scroll cho danh sách lớp học

--- DayHocTrucTuyen/Controllers/DefaultController.cs
+++ DayHocTrucTuyen/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using DayHocTrucTuyen.Areas.Courses.Controllers;
+﻿using DayHocTrucTuyen.Areas.Admin.Models;
+using DayHocTrucTuyen.Areas.Courses.Controllers;
 using DayHocTrucTuyen.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -34,7 +35,7 @@ namespace DayHocTrucTuyen.Controllers
 
         //Trang danh sách lớp học
         [AllowAnonymous]
-        public IActionResult Courses(string q)
+        public IActionResult Courses(string? q, int? offset, int? limit)
         {
             var maNd = "U000000";
             if (User.Identity.IsAuthenticated)
@@ -45,21 +46,67 @@ namespace DayHocTrucTuyen.Controllers
 
             //Lấy lớp học từ db và lớp học được đề xuất
             List<LopHoc> room = db.LopHocs.OrderByDescending(x => x.NgayTao).ToList();
-            var result = sugesst.getRoom(maNd);
+            var lst = sugesst.getRoom(maNd);
 
             //Gộp 2 danh sách đề xuất là lớp toàn hệ thống lại, lấy duy nhất giá trị
-            result.AddRange(room);
-            result = result.DistinctBy(x => x.MaLop).ToList();
+            lst.AddRange(room);
+            lst = lst.DistinctBy(x => x.MaLop).ToList();
 
             //Xử lý tìm kiếm
             if (!String.IsNullOrEmpty(q))
             {
-                result = result.Where(x => x.TenLop.ToLower().Contains(q.ToLower())).ToList();
+                lst = lst.Where(x => x.TenLop.ToLower().Contains(q.ToLower())).ToList();
             }
 
             ViewBag.Search = q;
 
-            return View(result);
+            if(offset == null || limit == null)
+            {
+                return View(new List<LopHoc>());
+            }
+
+            //Xử lý phân trang
+            var temp = PaginatedList<LopHoc>.Create(lst, offset ?? 0, limit ?? 10);
+
+            //Tạo list lớp học custom để trả về json
+            List<dynamic> result = new List<dynamic>();
+            foreach (var i in temp)
+            {
+                //Lấy tag của lớp gán vào list
+                List<dynamic> tempLst = new List<dynamic>();
+                var tag = i.getTag();
+                foreach (var t in tag)
+                {
+                    var temptag = new
+                    {
+                        maTag = t.MaTag,
+                        tenTag = t.TenTag
+                    };
+                    tempLst.Add(temptag);
+                }
+
+                //Custom lớp học
+                var owner = i.getOwner();
+                var item = new
+                {
+                    maLop = i.MaLop,
+                    tenLop = i.TenLop,
+                    loaiGiaTien = i.getTypeGiaTien(),
+                    giaTien = i.getGiaTien(),
+                    imgBg = i.getImage(),
+                    tag = tempLst,
+                    moTa = String.IsNullOrEmpty(i.MoTa) ? "Lớp chưa có mô tả!" : i.MoTa,
+                    ownerMa = owner.MaNd,
+                    ownerAvt = owner.getImageAvt(),
+                    ownerTen = owner.Ten,
+                    ownerHoTen = owner.getFullName(),
+                    thanhVien = i.getMembers(),
+                    camXuc = i.getSLCamXuc()
+                };
+                result.Add(item);
+            }
+
+            return Json(new { result });
         }
 
         //Trang liên hệ
