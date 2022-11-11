c6d286721d0272c0f8c9e10b54895186c8e0128c
Khánh Băng <trieukhanhbang123@gmail.com>
2022-11-11 23:32:00 +0700

    Thêm data cho biểu đồ thống kê ls giao dịch

--- DayHocTrucTuyen/Areas/User/Controllers/ManageController.cs
+++ DayHocTrucTuyen/Areas/User/Controllers/ManageController.cs
@@ -21,5 +21,39 @@ namespace DayHocTrucTuyen.Areas.User.Controllers
         {
             return View();
         }
+
+        //Lấy thông tin giao dịch trong năm hiện tại cho biểu đồ thống kê lịch sử giao dịch
+        [Authorize]
+        [HttpGet]
+        public IActionResult getTransHisOfYear()
+        {
+            var thisYear = DateTime.Now.Year;
+            var maNd = User.Claims.First().Value;
+
+            List<double> lstThu = new List<double>();
+            List<double> lstChi = new List<double>();
+            List<double> lstSoDu = new List<double>();
+
+            for(int i = 1; i <= 12; i++)
+            {
+                double tempThu = 0;
+                double tempChi = 0;
+                double tempSoDu = 0;
+                var temp = db.LichSuGiaoDiches.Where(x=>x.MaNd == maNd && x.ThoiGian.Year == thisYear && x.ThoiGian.Month == i).ToList();
+                
+                foreach(var ls in temp)
+                {
+                    if (ls.ThuVao) tempThu += ls.SoTien;
+                    else tempChi += ls.SoTien;
+                    tempSoDu = ls.SoDu;
+                }
+
+                lstThu.Add(tempThu);
+                lstChi.Add(tempChi);
+                lstSoDu.Add(tempSoDu);
+            }
+
+            return Json(new { thu = lstThu, chi = lstChi, sodu = lstSoDu });
+        }
     }
 }
--- DayHocTrucTuyen/wwwroot/Template/js/custom_statistic.js
+++ DayHocTrucTuyen/wwwroot/Template/js/custom_statistic.js
@@ -1,306 +1,316 @@
-﻿function init_echarts() {
-    if (typeof (echarts) === 'undefined') { return; }
-    var theme = {
-        color: [
-            '#26B99A', '#34495E', '#fa6342', '#3498DB',
-            '#8c6ad2', '#8abb6f', '#759c6a', '#bfd3b7'
-        ],
+﻿var theme = {
+    color: [
+        '#26B99A', '#3498DB', '#fa6342', '#34495E',
+        '#8c6ad2', '#8abb6f', '#759c6a', '#bfd3b7'
+    ],
 
-        title: {
-            itemGap: 8,
-            textStyle: {
-                fontWeight: 'normal',
-                color: '#535165'
-            }
-        },
+    title: {
+        itemGap: 8,
+        textStyle: {
+            fontWeight: 'normal',
+            color: '#535165'
+        }
+    },
 
-        dataRange: {
-            color: ['#1f610a', '#97b58d']
-        },
+    dataRange: {
+        color: ['#1f610a', '#97b58d']
+    },
 
-        toolbox: {
-            color: ['#408829', '#408829', '#408829', '#408829']
-        },
+    toolbox: {
+        color: ['#408829', '#408829', '#408829', '#408829']
+    },
 
-        tooltip: {
-            backgroundColor: 'rgba(0,0,0,0.5)',
-            axisPointer: {
-                type: 'line',
-                lineStyle: {
-                    color: '#408829',
-                    type: 'dashed'
-                },
-                crossStyle: {
-                    color: '#408829'
-                },
-                shadowStyle: {
-                    color: 'rgba(200,200,200,0.3)'
-                }
-            }
-        },
-
-        dataZoom: {
-            dataBackgroundColor: '#eee',
-            fillerColor: 'rgba(64,136,41,0.2)',
-            handleColor: '#408829'
-        },
-        grid: {
-            borderWidth: 0
-        },
-
-        categoryAxis: {
-            axisLine: {
-                lineStyle: {
-                    color: '#408829'
-                }
-            },
-            splitLine: {
-                lineStyle: {
-                    color: ['#eee']
-                }
-            }
-        },
-
-        valueAxis: {
-            axisLine: {
-                lineStyle: {
-                    color: '#408829'
-                }
-            },
-            splitArea: {
-                show: true,
-                areaStyle: {
-                    color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
-                }
-            },
-            splitLine: {
-                lineStyle: {
-                    color: ['#eee']
-                }
-            }
-        },
-        timeline: {
+    tooltip: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        axisPointer: {
+            type: 'line',
             lineStyle: {
+                color: '#408829',
+                type: 'dashed'
+            },
+            crossStyle: {
                 color: '#408829'
             },
-            controlStyle: {
-                normal: { color: '#408829' },
-                emphasis: { color: '#408829' }
+            shadowStyle: {
+                color: 'rgba(200,200,200,0.3)'
             }
-        },
-
-        k: {
-            itemStyle: {
-                normal: {
-                    color: '#68a54a',
-                    color0: '#a9cba2',
-                    lineStyle: {
-                        width: 1,
-                        color: '#408829',
-                        color0: '#86b379'
-                    }
-                }
-            }
-        },
-        map: {
-            itemStyle: {
-                normal: {
-                    areaStyle: {
-                        color: '#ddd'
-                    },
-                    label: {
-                        textStyle: {
-                            color: '#c12e34'
-                        }
-                    }
-                },
-                emphasis: {
-                    areaStyle: {
-                        color: '#99d2dd'
-                    },
-                    label: {
-                        textStyle: {
-                            color: '#c12e34'
-                        }
-                    }
-                }
-            }
-        },
-        force: {
-            itemStyle: {
-                normal: {
-                    linkStyle: {
-                        strokeColor: '#408829'
-                    }
-                }
-            }
-        },
-        chord: {
-            padding: 4,
-            itemStyle: {
-                normal: {
-                    lineStyle: {
-                        width: 1,
-                        color: 'rgba(128, 128, 128, 0.5)'
-                    },
-                    chordStyle: {
-                        lineStyle: {
-                            width: 1,
-                            color: 'rgba(128, 128, 128, 0.5)'
-                        }
-                    }
-                },
-                emphasis: {
-                    lineStyle: {
-                        width: 1,
-                        color: 'rgba(128, 128, 128, 0.5)'
-                    },
-                    chordStyle: {
-                        lineStyle: {
-                            width: 1,
-                            color: 'rgba(128, 128, 128, 0.5)'
-                        }
-                    }
-                }
-            }
-        },
-        gauge: {
-            startAngle: 225,
-            endAngle: -45,
-            axisLine: {
-                show: true,
-                lineStyle: {
-                    color: [[0.2, '#86b379'], [0.8, '#68a54a'], [1, '#408829']],
-                    width: 8
-                }
-            },
-            axisTick: {
-                splitNumber: 10,
-                length: 12,
-                lineStyle: {
-                    color: 'auto'
-                }
-            },
-            axisLabel: {
-                textStyle: {
-                    color: 'auto'
-                }
-            },
-            splitLine: {
-                length: 18,
-                lineStyle: {
-                    color: 'auto'
-                }
-            },
-            pointer: {
-                length: '90%',
-                color: 'auto'
-            },
-            title: {
-                textStyle: {
-                    color: '#333'
-                }
-            },
-            detail: {
-                textStyle: {
-                    color: 'auto'
-                }
-            }
-        },
-        textStyle: {
-            fontFamily: 'Arial, Verdana, sans-serif'
         }
-    };
+    },
 
-    if ($('#echart_line').length) {
-        var echartLine = echarts.init(document.getElementById('echart_line'), theme);
+    dataZoom: {
+        dataBackgroundColor: '#eee',
+        fillerColor: 'rgba(64,136,41,0.2)',
+        handleColor: '#408829'
+    },
+    grid: {
+        borderWidth: 0
+    },
 
-        echartLine.setOption({
-            title: {
-                text: '',
-                subtext: 'Biểu đồ thống kê'
-            },
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                x: 220,
-                y: 40,
-                data: ['Thu', 'Chi', 'Số dư']
-            },
-            toolbox: {
-                show: true,
-                feature: {
-                    magicType: {
-                        show: true,
-                        title: {
-                            line: 'Dòng',
-                            bar: 'Cột',
-                            stack: 'Xếp chồng',
-                            tiled: 'Phân nhóm'
-                        },
-                        type: ['line', 'bar', 'stack', 'tiled']
-                    },
-                    restore: {
-                        show: true,
-                        title: "Làm mới"
-                    },
-                    saveAsImage: {
-                        show: true,
-                        title: "Lưu ảnh"
+    categoryAxis: {
+        axisLine: {
+            lineStyle: {
+                color: '#408829'
+            }
+        },
+        splitLine: {
+            lineStyle: {
+                color: ['#eee']
+            }
+        }
+    },
+
+    valueAxis: {
+        axisLine: {
+            lineStyle: {
+                color: '#408829'
+            }
+        },
+        splitArea: {
+            show: true,
+            areaStyle: {
+                color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
+            }
+        },
+        splitLine: {
+            lineStyle: {
+                color: ['#eee']
+            }
+        }
+    },
+    timeline: {
+        lineStyle: {
+            color: '#408829'
+        },
+        controlStyle: {
+            normal: { color: '#408829' },
+            emphasis: { color: '#408829' }
+        }
+    },
+
+    k: {
+        itemStyle: {
+            normal: {
+                color: '#68a54a',
+                color0: '#a9cba2',
+                lineStyle: {
+                    width: 1,
+                    color: '#408829',
+                    color0: '#86b379'
+                }
+            }
+        }
+    },
+    map: {
+        itemStyle: {
+            normal: {
+                areaStyle: {
+                    color: '#ddd'
+                },
+                label: {
+                    textStyle: {
+                        color: '#c12e34'
                     }
                 }
             },
-            calculable: true,
-            xAxis: [{
-                type: 'category',
-                boundaryGap: false,
-                data: ['Thg 1', 'Thg 2', 'Thg 3', 'Thg 4', 'Thg 5', 'Thg 6', 'Thg 7', 'Thg 8', 'Thg 9', 'Thg 10', 'Thg 11', 'Thg 12']
-            }],
-            yAxis: [{
-                type: 'value'
-            }],
-            series: [{
-                name: 'Số dư',
-                type: 'line',
-                smooth: true,
-                itemStyle: {
-                    normal: {
-                        areaStyle: {
-                            type: 'default'
-                        }
-                    }
+            emphasis: {
+                areaStyle: {
+                    color: '#99d2dd'
                 },
-                data: [10, 12, 21, 54, 260, 830, 710, 200, 300, 234, 345, 44]
-            }, {
-                name: 'Thu',
-                type: 'line',
-                smooth: true,
-                itemStyle: {
-                    normal: {
-                        areaStyle: {
-                            type: 'default'
-                        }
+                label: {
+                    textStyle: {
+                        color: '#c12e34'
                     }
+                }
+            }
+        }
+    },
+    force: {
+        itemStyle: {
+            normal: {
+                linkStyle: {
+                    strokeColor: '#408829'
+                }
+            }
+        }
+    },
+    chord: {
+        padding: 4,
+        itemStyle: {
+            normal: {
+                lineStyle: {
+                    width: 1,
+                    color: 'rgba(128, 128, 128, 0.5)'
                 },
-                data: [30, 182, 434, 791, 390, 30, 10, 33, 312, 355, 345, 233]
-            }, {
-                name: 'Chi',
-                type: 'line',
-                smooth: true,
-                itemStyle: {
-                    normal: {
-                        areaStyle: {
-                            type: 'default'
-                        }
+                chordStyle: {
+                    lineStyle: {
+                        width: 1,
+                        color: 'rgba(128, 128, 128, 0.5)'
                     }
+                }
+            },
+            emphasis: {
+                lineStyle: {
+                    width: 1,
+                    color: 'rgba(128, 128, 128, 0.5)'
                 },
-                data: [1320, 1132, 601, 234, 120, 90, 20, 43, 56, 767, 323, 44]
-            }]
-        });
+                chordStyle: {
+                    lineStyle: {
+                        width: 1,
+                        color: 'rgba(128, 128, 128, 0.5)'
+                    }
+                }
+            }
+        }
+    },
+    gauge: {
+        startAngle: 225,
+        endAngle: -45,
+        axisLine: {
+            show: true,
+            lineStyle: {
+                color: [[0.2, '#86b379'], [0.8, '#68a54a'], [1, '#408829']],
+                width: 8
+            }
+        },
+        axisTick: {
+            splitNumber: 10,
+            length: 12,
+            lineStyle: {
+                color: 'auto'
+            }
+        },
+        axisLabel: {
+            textStyle: {
+                color: 'auto'
+            }
+        },
+        splitLine: {
+            length: 18,
+            lineStyle: {
+                color: 'auto'
+            }
+        },
+        pointer: {
+            length: '90%',
+            color: 'auto'
+        },
+        title: {
+            textStyle: {
+                color: '#333'
+            }
+        },
+        detail: {
+            textStyle: {
+                color: 'auto'
+            }
+        }
+    },
+    textStyle: {
+        fontFamily: 'Arial, Verdana, sans-serif'
     }
+};
+
+function setChartTransHis(thu, chi, sodu) {
+    if (typeof (echarts) === 'undefined') { return; }
+    var echartLine = echarts.init(document.getElementById('echart_line'), theme);
+
+    echartLine.setOption({
+        title: {
+            text: '',
+            subtext: 'Biểu đồ thống kê'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            x: 220,
+            y: 40,
+            data: ['Thu', 'Chi', 'Số dư']
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                magicType: {
+                    show: true,
+                    title: {
+                        line: 'Dòng',
+                        bar: 'Cột',
+                        stack: 'Xếp chồng',
+                        tiled: 'Phân nhóm'
+                    },
+                    type: ['line', 'bar', 'stack', 'tiled']
+                },
+                restore: {
+                    show: true,
+                    title: "Làm mới"
+                },
+                saveAsImage: {
+                    show: true,
+                    title: "Lưu ảnh"
+                }
+            }
+        },
+        calculable: true,
+        xAxis: [{
+            type: 'category',
+            boundaryGap: false,
+            data: ['Thg 1', 'Thg 2', 'Thg 3', 'Thg 4', 'Thg 5', 'Thg 6', 'Thg 7', 'Thg 8', 'Thg 9', 'Thg 10', 'Thg 11', 'Thg 12']
+        }],
+        yAxis: [{
+            type: 'value'
+        }],
+        series: [{
+            name: 'Số dư',
+            type: 'bar',
+            smooth: true,
+            itemStyle: {
+                normal: {
+                    areaStyle: {
+                        type: 'default'
+                    }
+                }
+            },
+            data: sodu
+        }, {
+            name: 'Thu',
+            type: 'bar',
+            smooth: true,
+            itemStyle: {
+                normal: {
+                    areaStyle: {
+                        type: 'default'
+                    }
+                }
+            },
+            data: thu
+        }, {
+            name: 'Chi',
+            type: 'bar',
+            smooth: true,
+            itemStyle: {
+                normal: {
+                    areaStyle: {
+                        type: 'default'
+                    }
+                }
+            },
+            data: chi
+        }]
+    });
 }
 
 $(document).ready(function () {
-    init_echarts();
+    //Nếu đang mở trang xem lịch sử giao dịch
+    if ($('#echart_transhistory').length) {
+        $.ajax({
+            url: '/user/manage/gettranshisofyear',
+            type: 'GET',
+            success: function (data) {
+                setChartTransHis(data.thu, data.chi, data.sodu)
+            },
+            error: function () {
+                getThongBao('error', 'Lỗi', 'Không thể gửi yêu cầu về máy chủ !')
+            }
+        })
+    }
 });
